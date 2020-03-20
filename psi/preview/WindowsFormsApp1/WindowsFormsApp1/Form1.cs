6f2eeded33a75c0b964f6f2e0858c07ad42364d8
Chris <cchen@futuredial.com>
2020-03-19 20:19:00 -0700

    PSI project preview demo

---
+++ psi/preview/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        System.Threading.AutoResetEvent quit_evt = new System.Threading.AutoResetEvent(false);
+        Task t = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void load_image()
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() => { load_image(); }));
+            }
+            else
+            {
+                try
+                {
+                    WebClient wc = new WebClient();
+                    byte[] data = wc.DownloadData("http://192.168.5.39:5000/preview");
+                    using (MemoryStream m = new MemoryStream(data))
+                    {
+                        pictureBox1.Image = Image.FromStream(m);
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load_image();
+            t= Task.Run(() =>
+            {
+                while (!quit_evt.WaitOne(5))
+                {
+                    load_image();
+                    //System.Threading.Thread.Sleep(5);
+                }
+            });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            quit_evt.Set();
+            t.Wait(1000);
+            Application.Exit();
+        }
+    }
+}
---
+++ psi/preview/preview.py
@@ -0,0 +1,76 @@
+import picamera
+from flask import Flask, request, send_file
+import time
+import io
+from PIL import Image
+import threading
+import logging
+import sys
+
+app = Flask(__name__)
+quit_event = threading.Event()
+save_image_event = threading.Event()
+save_complete_event = threading.Event()
+image_ready = io.BytesIO()
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+def preview():
+    global image_ready
+    logging.info("preview: thread is starting...")
+    stream = io.BytesIO()
+    with picamera.PiCamera() as camera:
+        camera.ISO = 50
+        camera.start_preview()
+        time.sleep(2)
+        for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+            stream.seek(0)
+            image = Image.open(stream)
+            if save_image_event.is_set():
+                # with open("foo.jpg", "w") as f:                
+                #     image.save(f)
+                if image_ready is None or image_ready.closed:
+                    image_ready = io.BytesIO()
+                else:
+                    image_ready.seek(0)
+                    image_ready.truncate()
+                image.save(image_ready, format='JPEG')
+                save_complete_event.set()
+                save_image_event.clear()
+            stream.seek(0)
+            stream.truncate()
+            if quit_event.is_set():
+                break
+    logging.info("preview: thread is terminated")
+
+@app.route('/')
+def hello_world():
+    # return 'post data to http://10.1.1.154:5000/sga'
+    #return render_template('home.html')
+    return "Hello, World!"
+
+@app.route('/preview')
+def preview_getimage():
+    save_image_event.set()
+    save_complete_event.wait()
+    save_complete_event.clear()
+    image_ready.seek(0)
+    return send_file(image_ready, mimetype='image/jpeg')
+
+@app.route('/shutdown')
+def shutdown():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    logging.info("main: is terminating...")
+    return 'Server shutting down...'
+
+if __name__ == '__main__':
+    logging.info("main: is starting.")
+    t = threading.Thread(target=preview, daemon=True)
+    t.start()
+    app.run(host='0.0.0.0')
+    logging.info("main: shutdown preview thread.")
+    quit_event.set()
+    t.join()
+    logging.info("main: is terminated.")
